4694d4e276ea404857a703103ac493fbe2abcee4
Hemanh07 <raghuhemanth008@gmail.com>
2023-12-21 21:07:02 +0530

    dynamic text box

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const chatdisplay=document.getElementById("chatdisplay");
+const chatboxcontainer=document.getElementById("chatboxcontainer");
 const sentbutton=document.getElementById("sentbutton");
 const chatbox=document.getElementById("chatbox");
 const emo=document.getElementById("emo");
 const form=document.getElementById("typebox")
+const addbutton=document.getElementById("addbutton");
+/* addbutton.addEventListener("click",(event)=>{
+    const newbox=document.createElement("div");
+    newbox.innerText="";
+    chatboxcontainer.appendChild(newbox);
+
+}); */
 sentbutton.addEventListener("click",(event)=>{
     const chatboxs=document.getElementById("chatbox").value;
     const e=document.getElementById("emo").value;
-    chatdisplay.innerText=chatboxs+e;
-    document.getElementById("chatbox").value=' ';
+    const newbox=document.createElement("div");
+    newbox.innerText=chatboxs+e;
+    chatboxcontainer.appendChild(newbox)
+    
+    
+    document.getElementById("chatbox").value='';
     document.getElementById("emo").value='';
 }
 );
